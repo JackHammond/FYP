a507f6bc84aeea74f722d9528b426dc8ba11efa5
Jack Hammond <1617546@brunel.ac.uk>
2019-11-20 15:08:20 +0000

    Microservice and Monolithic templates created

---
+++ Microservice/ms_flutter_front_end/lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'API.dart';
+import 'User.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Http App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyListScreen(),
+    );
+  }
+}
+
+class MyListScreen extends StatefulWidget {
+  @override
+  createState() => _MyListScreenState();
+}
+
+class _MyListScreenState extends State {
+  var users = new List<User>();
+
+  _getUsers() {
+    API.getUsers().then((response) {
+      setState(() {
+        Iterable list = json.decode(response.body);
+        users = list.map((model) => User.fromJson(model)).toList();
+      });
+    });
+  }
+
+  initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  dispose() {
+    super.dispose();
+  }
+
+  @override
+  build(context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("User List"),
+        ),
+        body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text(users[index].name));
+          },
+        ));
+  }
+}
