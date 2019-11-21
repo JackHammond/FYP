733842dc9b868cc9dbc1a8f0760ddbc653669be4
Jack Hammond <1617546@brunel.ac.uk>
2019-11-21 18:09:52 +0000

    second page with routing, also catalog service

---
+++ Monolithic/mono_flutter_front_end/lib/API.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+const baseUrl = "https://jsonplaceholder.typicode.com";
+
+class API {
+  static Future getUsers() {
+    var url = baseUrl + "/users";
+    return http.get(url);
+  }
+  static Future getCatalog(){
+    var url = baseUrl + "/albums/1/photos";
+    return http.get(url);
+  }
+}
