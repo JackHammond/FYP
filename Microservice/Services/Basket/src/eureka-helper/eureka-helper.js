f149021c5a451ce57b6c1a01eddcb76ab76e1ac5
jackhammond <jackhammond_@hotmail.co.uk>
2020-02-02 20:17:49 +0000

    Exposed Infrastructure services externally, services now connect and can be queried successfully

--- Microservice/Services/Basket/src/eureka-helper/eureka-helper.js
+++ Microservice/Services/Basket/src/eureka-helper/eureka-helper.js
@@ -1,8 +1,9 @@
 const Eureka = require('eureka-js-client').Eureka;
 const eurekaHost = (process.env.EUREKA_CLIENT_SERVICEURL_DEFAULTZONE || 'eureka-service');
 const eurekaPort = 8761;
-const hostName = (process.env.HOSTNAME || 'eureka-service')
-const ipAddr = 'eureka-service';
+//const hostName = (process.env.HOSTNAME || 'eureka-service')
+const hostName = ('basket-service')
+const ipAddr = 'basket-service';
 
 exports.registerWithEureka = function (appName, PORT) {
     const client = new Eureka({
--- Microservice/Services/Catalog/database.js
+++ Microservice/Services/Catalog/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 async function connect(){
-      await mongoose.connect('mongodb://mongo_catalog:27017/ms-catalog-flutter-db',{
-//      await mongoose.connect('mongodb://localhost:27017/ms-catalog-flutter-db',{
+//      await mongoose.connect('mongodb://mongo_catalog:27017/ms-catalog-flutter-db',{
+      await mongoose.connect('mongodb://mongo-catalog:27017/ms-catalog-flutter-db',{
         useNewUrlParser: true, useFindAndModify:false
     });
     console.log('Catalog Database: Connected');
