463007cf1b66d659de4b0920590dd8959641fec1
root <root@testing-mono-auto-deploy.europe-west2-c.c.fyp-project-258513.internal>
2020-02-11 19:24:02 +0000

    updated flutter end point to point to mono vm - could be scaled by changing to dns server of database vm

--- Monolithic/mono_flutter_front_end/lib/main.dart
+++ Monolithic/mono_flutter_front_end/lib/main.dart
@@ -31,7 +31,8 @@ class _HomePageState extends State<HomePage> {
 
   getProducts() async {
     http.Response response =
-        await http.get('http://localhost:4000/api/catalog');
+        await http.get('http://34.89.19.193:4000/api/catalog',
+        headers: {'Access-Control-Allow-Origin': '*'});
     data = json.decode(response.body);
     setState(() {
       productData = data['products'];
@@ -39,7 +40,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   getReviews() async {
-    http.Response response = await http.get('http://localhost:4000/api/review');
+    http.Response response = await http.get('http://34.89.19.193:4000/api/review');
     data = json.decode(response.body);
     setState(() {
       productReview = data['reviews'];
@@ -47,7 +48,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   getBasket() async {
-    http.Response response = await http.get('http://localhost:4000/api/basket');
+    http.Response response = await http.get('http://34.89.19.193:4000/api/basket');
     data = json.decode(response.body);
     setState(() {
       productBasket = data['baskets'];
@@ -65,7 +66,7 @@ class _HomePageState extends State<HomePage> {
 
   createRating(String productID, String rating) async {
     http.Response response = await http.post(
-        'http://localhost:4000/api/review/create',
+        'http://34.89.19.193:4000/api/review/create',
         body: {"_id": productID, "productRating": rating});
     getProducts(); //this will refresh the product catalog list
     getReviews(); //this will refresh the review list
@@ -83,7 +84,7 @@ class _HomePageState extends State<HomePage> {
   createBasket(String userID, String productID) async {
     basketItems.add(productID);
     http.Response response = await http
-        .post('http://localhost:4000/api/basket/create', body: {
+        .post('http://34.89.19.193:4000/api/basket/create', body: {
       "user_ID": userID,
       "savedProduct_IDs": json.encode(basketItems)
     });
@@ -94,7 +95,7 @@ class _HomePageState extends State<HomePage> {
     basketItems.add(productID);
     print(basketItems.toString());
     http.Response response = await http.put(
-        'http://localhost:4000/api/basket/update',
+        'http://34.89.19.193:4000/api/basket/update',
         body: {"_id": basketID, "savedProduct_IDs": json.encode(basketItems)});
     getBasket();
   }
@@ -106,7 +107,7 @@ class _HomePageState extends State<HomePage> {
     }
     print("Updated Basket: " + basketItems.toString());
     http.Response response = await http.put(
-        'http://localhost:4000/api/basket/update',
+        'http://34.89.19.193:4000/api/basket/update',
         body: {"_id": basketID, "savedProduct_IDs": json.encode(basketItems)});
     getBasket();
   }
@@ -114,7 +115,7 @@ class _HomePageState extends State<HomePage> {
   //monolithic logic for REVIEW - post average review rating
   updateProductRating(String listitem, String rating) async {
     http.Response response = await http.put(
-        "http://localhost:4000/api/catalog/rating",
+        "http://34.89.19.193:4000/api/catalog/rating",
         body: {"_id": listitem, "productRating": rating});
   }
 
